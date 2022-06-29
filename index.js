8fcf0d8980f5afc8863a1ed26138c73d012c4d11
Dorian Loret <dorian.loret20@gmail.com>
2022-06-29 18:25:11 +0200

    end

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-let countries = [];
+let countriesData = [];
+let sortMethod = "maxToMin";
 const container = document.querySelector("body > div.countries-container");
+const btnSort = document.querySelectorAll(".btnSort");
 
 async function fetchCountries() {
   await fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
-    .then((data) => (countries = data));
+    .then((data) => (countriesData = data));
 
-  console.log(countries);
+  console.log(countriesData);
+  countriesDisplay();
 }
 
-const countryDisplay = async () => {
-  await fetchCountries();
-
-  container.innerHTML = countries
+function countriesDisplay() {
+  container.innerHTML = countriesData
+    .filter((country) =>
+      country.translations.fra.common
+        .toLowerCase()
+        .includes(inputSearch.value.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortMethod === "maxToMin") {
+        return b.population - a.population;
+      } else if (sortMethod === "minToMax") {
+        return a.population - b.population;
+      } else if (sortMethod === "alpha") {
+        return a.translations.fra.common.localeCompare(
+          b.translations.fra.common
+        );
+      }
+    })
+    .slice(0, inputRange.value)
     .map(
       (country) =>
         `
         <div class="card"> 
-        <img src=${country.flags.png} alt=${country.translations.fra.common}>
+        <img src=${country.flags.svg} alt=${country.translations.fra.common}>
         <h3>${country.translations.fra.common}</h3>
         <h4>${country.capital}</h4>
-        <h5>Population : ${country.population}
+        <p>Population : ${country.population.toLocaleString()}</p>
         </div>
         `
     )
     .join("");
-};
+}
 
-countryDisplay();
+window.addEventListener("load", fetchCountries);
+inputSearch.addEventListener("input", countriesDisplay);
 
-// 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
-// coutry.name.includes(inputSearch.value);
+inputRange.addEventListener("input", () => {
+  countriesDisplay();
+  rangeValue.textContent = inputRange.value;
+});
 
-// 6 - Avec la méthode Slice gérer le nombre de pays affichés (inputRange.value)
-
-// 7 - Gérer les 3 boutons pour trier (méthode sort()) les pays
+btnSort.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    sortMethod = e.target.id;
+    countriesDisplay();
+  });
+});
